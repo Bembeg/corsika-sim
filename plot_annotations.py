35f54b0b1f857503706db7d86f076a64fd6499a1
Radek Privara <radek.privara@cern.ch>
2025-10-25 08:21:59 +0200

    Listdir instead of manual def

--- plot_annotations.py
+++ plot_annotations.py
@@ -3,7 +3,7 @@
 
 from typing import NamedTuple
 import matplotlib.pyplot as plt
-
+import os
 
 # Class to hold function cost (cst), function name (fn) and its corresponding library (lib)
 class Result(NamedTuple):
@@ -12,18 +12,12 @@ class Result(NamedTuple):
     lib: str
 
 
-# Callgraph annotation outputs
-outputs = ["output/pdg22_E10_prof/annotate.txt",
-        "output/pdg22_E100_prof/annotate.txt",
-        "output/pdg22_E500_prof/annotate.txt",
-        "output/pdg2212_E10_prof/annotate.txt",
-        "output/pdg2212_E100_prof/annotate.txt",
-        "output/pdg2212_E500_prof/annotate.txt"]
+# Iterate over callgrind annotate outputs
+for sim in os.listdir("output/"):
+    print("Dir", sim)
 
-# Override for single output and debugging
-# outputs = ["output/pdg2212_E500_prof/annotate.txt"]
+    output = "output/" + sim + "/annotate.txt"
 
-for output in outputs:
     print("Processing output '", output, "'", sep="")
 
     # Open output file and read lines
@@ -38,13 +32,13 @@ for output in outputs:
 
     for line in lines:
         if ("TOTALS" in line):
-            run += 1 
+            run += 1
             results.append([])
             continue
 
         if ("=" in line) or (len(line) < 5):
             continue
-        
+
         # Parse cost
         cst = float(line[line.find("(")+1 : line.find(")")-1].strip())
 
@@ -53,7 +47,7 @@ for output in outputs:
             lib = "-"
         else:
             lib = line[line.rfind("/")+1 : line.find("]")].strip()
-        
+
         # Parse function name
         fn = line[line.find(")")+1 : line.find("[")].strip()
         if (fn.find("(") > 0):
@@ -70,14 +64,14 @@ for output in outputs:
     n_runs = run+1
     n_fct = len(results[0])
     print(" - processed", n_runs, "runs, each with", n_fct, "functions")
-    
+
     # Check if all rankings are identical
     print(" - checking if rankings match across runs")
-    
+
     # Number of mismatches
     mismatch = 0
-    
-    for f in range(n_fct):
+
+    for f in range(round(n_fct/2)):
         ref_f = results[0][f].fn
         for r in range(n_runs):
             # Detect diff between ref function (run0) and current function at the same position
@@ -94,7 +88,7 @@ for output in outputs:
     # Parse simulation label
     plot_name = output.split("/")[1]
     label = plot_name.replace("_prof", "").replace("pdg2212","proton").replace("pdg22","gamma")
-    
+
     # Plot functions per run
     fig, ax = plt.subplots()
     # Plot size
@@ -109,7 +103,7 @@ for output in outputs:
     ax.set(xticks=range(1, n_runs+1))
     # Grid
     ax.grid(True, linestyle="dashed", linewidth=0.5)
-    
+
     # Plot individual runs
     for r in range(n_runs):
         # Collect costs and names of functions
@@ -117,13 +111,13 @@ for output in outputs:
         fct = [fct.fn for fct in results[r]]
         # Plot this run
         ax.plot(range(len(cst)), cst, label="run"+str(r)+" ("+str(round(sum(cst),1))+"%)", linewidth=1, linestyle="solid", marker="x")
-    
+
     # List functions and corresponding libs
     for f in range(n_fct):
         ax.annotate(str(f+1) + ": " + results[0][f].fn[0:40] + "   [" + results[0][f].lib[0:30] + "]", (0.12, 0.30-f*0.03), xycoords="figure fraction", fontsize=8)
-    
+
     # Draw legend
     ax.legend(fontsize=8)
-    
+
     # Save plot
     plt.savefig("plots/" + plot_name + ".png", dpi=600)
