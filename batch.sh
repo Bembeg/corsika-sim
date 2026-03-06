097c531c2e8375fcbd4bbdca19eb92507f54c406
Radek Privara <radek.privara@cern.ch>
2026-03-06 11:38:37 +0100

    update batch script

--- batch.sh
+++ batch.sh
@@ -1,24 +1,26 @@
 source ../source.sh
 
 # Run simulations
-CONFIG="interp"
-# Standard injection height
-bash run.sh $CONFIG 22 1e3 112750 0 1 10
-bash run.sh $CONFIG 22 1e4 112750 0 1 10
-bash run.sh $CONFIG 22 1e5 112750 0 1 2
-bash run.sh $CONFIG 2212 1e3 112750 0 1 10
-bash run.sh $CONFIG 2212 1e4 112750 0 1 10
-bash run.sh $CONFIG 2212 1e5 112750 0 1 2
-# Low injection height
-bash run.sh $CONFIG 22 1e3 6900 0 1 10
-bash run.sh $CONFIG 22 1e4 6900 0 1 10
-bash run.sh $CONFIG 22 1e5 6900 0 1 2
-bash run.sh $CONFIG 2212 1e3 6900 0 1 10
-bash run.sh $CONFIG 2212 1e4 6900 0 1 10
-bash run.sh $CONFIG 2212 1e5 6900 0 1 2
+if true; then
+    CONFIG="interp"
+    # Standard injection height
+    bash run.sh $CONFIG 22 1e3 112750 0 1 1000
+    bash run.sh $CONFIG 22 1e4 112750 0 1 500
+    bash run.sh $CONFIG 22 1e5 112750 0 1 100
+    bash run.sh $CONFIG 2212 1e3 112750 0 1 1000
+    bash run.sh $CONFIG 2212 1e4 112750 0 1 500
+    bash run.sh $CONFIG 2212 1e5 112750 0 1 100
+    # Low injection height
+    bash run.sh $CONFIG 22 1e3 6900 0 1 1000
+    bash run.sh $CONFIG 22 1e4 6900 0 1 500
+    bash run.sh $CONFIG 22 1e5 6900 0 1 100
+    bash run.sh $CONFIG 2212 1e3 6900 0 1 1000
+    bash run.sh $CONFIG 2212 1e4 6900 0 1 500
+    bash run.sh $CONFIG 2212 1e5 6900 0 1 100
+fi
 
 # Merge outputs
-if false; then
+if true; then
     # Standard injection height
     python3 merge_outputs.py pdg22_E1e3_inj112750_z0_interp
     python3 merge_outputs.py pdg22_E1e4_inj112750_z0_interp
@@ -48,13 +50,19 @@ if false; then
 fi
 
 # Analysis
-if false; then
+if true; then
     # Standard injection height
-    python3 pdg22_E1e3_stdAlt pdg22_E1e3_inj112750_z0_expon pdg22_E1e3_inj112750_z0_interp
-    python3 pdg22_E1e4_stdAlt pdg22_E1e4_inj112750_z0_expon pdg22_E1e4_inj112750_z0_interp
-    python3 pdg22_E1e5_stdAlt pdg22_E1e5_inj112750_z0_expon pdg22_E1e5_inj112750_z0_interp
+    python3 analysis.py pdg22_E1e3_stdAlt pdg22_E1e3_inj112750_z0_expon pdg22_E1e3_inj112750_z0_interp
+    python3 analysis.py pdg22_E1e4_stdAlt pdg22_E1e4_inj112750_z0_expon pdg22_E1e4_inj112750_z0_interp
+    python3 analysis.py pdg22_E1e5_stdAlt pdg22_E1e5_inj112750_z0_expon pdg22_E1e5_inj112750_z0_interp
+    python3 analysis.py pdg2212_E1e3_stdAlt pdg2212_E1e3_inj112750_z0_expon pdg2212_E1e3_inj112750_z0_interp
+    python3 analysis.py pdg2212_E1e4_stdAlt pdg2212_E1e4_inj112750_z0_expon pdg2212_E1e4_inj112750_z0_interp
+    python3 analysis.py pdg2212_E1e5_stdAlt pdg2212_E1e5_inj112750_z0_expon pdg2212_E1e5_inj112750_z0_interp
     # Low injection height
-    python3 pdg22_E1e3_lowAlt pdg22_E1e3_inj6900_z0_expon pdg22_E1e3_inj6900_z0_interp
-    python3 pdg22_E1e4_lowAlt pdg22_E1e4_inj6900_z0_expon pdg22_E1e4_inj6900_z0_interp
-    python3 pdg22_E1e5_lowAlt pdg22_E1e5_inj6900_z0_expon pdg22_E1e5_inj6900_z0_interp
+    python3 analysis.py pdg22_E1e3_lowAlt pdg22_E1e3_inj6900_z0_expon pdg22_E1e3_inj6900_z0_interp
+    python3 analysis.py pdg22_E1e4_lowAlt pdg22_E1e4_inj6900_z0_expon pdg22_E1e4_inj6900_z0_interp
+    python3 analysis.py pdg22_E1e5_lowAlt pdg22_E1e5_inj6900_z0_expon pdg22_E1e5_inj6900_z0_interp
+    python3 analysis.py pdg2212_E1e3_lowAlt pdg2212_E1e3_inj6900_z0_expon pdg2212_E1e3_inj6900_z0_interp
+    python3 analysis.py pdg2212_E1e4_lowAlt pdg2212_E1e4_inj6900_z0_expon pdg2212_E1e4_inj6900_z0_interp
+    python3 analysis.py pdg2212_E1e5_lowAlt pdg2212_E1e5_inj6900_z0_expon pdg2212_E1e5_inj6900_z0_interp
 fi
