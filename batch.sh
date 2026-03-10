9c6f869589fb654b0399986bf4ff82af5a1940c1
Radek Privara <radek.privara@upol.cz>
2026-03-10 14:39:44 +0100

    batch script for inclined sim

--- batch.sh
+++ batch.sh
@@ -3,66 +3,24 @@ source ../source.sh
 # Run simulations
 if true; then
     CONFIG="interp"
-    # Standard injection height
-    bash run.sh $CONFIG 22 1e3 112750 0 1 1000
-    bash run.sh $CONFIG 22 1e4 112750 0 1 500
-    bash run.sh $CONFIG 22 1e5 112750 0 1 100
-    bash run.sh $CONFIG 2212 1e3 112750 0 1 1000
-    bash run.sh $CONFIG 2212 1e4 112750 0 1 500
-    bash run.sh $CONFIG 2212 1e5 112750 0 1 100
-    # Low injection height
-    bash run.sh $CONFIG 22 1e3 6900 0 1 1000
-    bash run.sh $CONFIG 22 1e4 6900 0 1 500
-    bash run.sh $CONFIG 22 1e5 6900 0 1 100
-    bash run.sh $CONFIG 2212 1e3 6900 0 1 1000
-    bash run.sh $CONFIG 2212 1e4 6900 0 1 500
-    bash run.sh $CONFIG 2212 1e5 6900 0 1 100
+    bash run.sh $CONFIG 2212 1e3 112750 60 1 1000
+    bash run.sh $CONFIG 2212 1e4 112750 60 1 1000
+    bash run.sh $CONFIG 2212 1e5 112750 60 1 500
 fi
 
 # Merge outputs
-if true; then
-    # Standard injection height
-    python3 merge_outputs.py pdg22_E1e3_inj112750_z0_interp
-    python3 merge_outputs.py pdg22_E1e4_inj112750_z0_interp
-    python3 merge_outputs.py pdg22_E1e5_inj112750_z0_interp
-    python3 merge_outputs.py pdg2212_E1e3_inj112750_z0_interp
-    python3 merge_outputs.py pdg2212_E1e4_inj112750_z0_interp
-    python3 merge_outputs.py pdg2212_E1e5_inj112750_z0_interp
-    python3 merge_outputs.py pdg22_E1e3_inj112750_z0_expon
-    python3 merge_outputs.py pdg22_E1e4_inj112750_z0_expon
-    python3 merge_outputs.py pdg22_E1e5_inj112750_z0_expon
-    python3 merge_outputs.py pdg2212_E1e3_inj112750_z0_expon
-    python3 merge_outputs.py pdg2212_E1e4_inj112750_z0_expon
-    python3 merge_outputs.py pdg2212_E1e5_inj112750_z0_expon
-    # Low injection height
-    python3 merge_outputs.py pdg22_E1e3_inj6900_z0_interp
-    python3 merge_outputs.py pdg22_E1e4_inj6900_z0_interp
-    python3 merge_outputs.py pdg22_E1e5_inj6900_z0_interp
-    python3 merge_outputs.py pdg2212_E1e3_inj6900_z0_interp
-    python3 merge_outputs.py pdg2212_E1e4_inj6900_z0_interp
-    python3 merge_outputs.py pdg2212_E1e5_inj6900_z0_interp
-    python3 merge_outputs.py pdg22_E1e3_inj6900_z0_expon
-    python3 merge_outputs.py pdg22_E1e4_inj6900_z0_expon
-    python3 merge_outputs.py pdg22_E1e5_inj6900_z0_expon
-    python3 merge_outputs.py pdg2212_E1e3_inj6900_z0_expon
-    python3 merge_outputs.py pdg2212_E1e4_inj6900_z0_expon
-    python3 merge_outputs.py pdg2212_E1e5_inj6900_z0_expon
+if false; then
+    python3 merge_outputs.py pdg2212_E1e3_inj112750_z60_interp
+    python3 merge_outputs.py pdg2212_E1e4_inj112750_z60_interp
+    python3 merge_outputs.py pdg2212_E1e5_inj112750_z60_interp
+    python3 merge_outputs.py pdg2212_E1e3_inj112750_z60_expon
+    python3 merge_outputs.py pdg2212_E1e4_inj112750_z60_expon
+    python3 merge_outputs.py pdg2212_E1e5_inj112750_z60_expon
 fi
 
 # Analysis
-if true; then
-    # Standard injection height
-    python3 analysis.py pdg22_E1e3_stdAlt pdg22_E1e3_inj112750_z0_expon pdg22_E1e3_inj112750_z0_interp
-    python3 analysis.py pdg22_E1e4_stdAlt pdg22_E1e4_inj112750_z0_expon pdg22_E1e4_inj112750_z0_interp
-    python3 analysis.py pdg22_E1e5_stdAlt pdg22_E1e5_inj112750_z0_expon pdg22_E1e5_inj112750_z0_interp
-    python3 analysis.py pdg2212_E1e3_stdAlt pdg2212_E1e3_inj112750_z0_expon pdg2212_E1e3_inj112750_z0_interp
-    python3 analysis.py pdg2212_E1e4_stdAlt pdg2212_E1e4_inj112750_z0_expon pdg2212_E1e4_inj112750_z0_interp
-    python3 analysis.py pdg2212_E1e5_stdAlt pdg2212_E1e5_inj112750_z0_expon pdg2212_E1e5_inj112750_z0_interp
-    # Low injection height
-    python3 analysis.py pdg22_E1e3_lowAlt pdg22_E1e3_inj6900_z0_expon pdg22_E1e3_inj6900_z0_interp
-    python3 analysis.py pdg22_E1e4_lowAlt pdg22_E1e4_inj6900_z0_expon pdg22_E1e4_inj6900_z0_interp
-    python3 analysis.py pdg22_E1e5_lowAlt pdg22_E1e5_inj6900_z0_expon pdg22_E1e5_inj6900_z0_interp
-    python3 analysis.py pdg2212_E1e3_lowAlt pdg2212_E1e3_inj6900_z0_expon pdg2212_E1e3_inj6900_z0_interp
-    python3 analysis.py pdg2212_E1e4_lowAlt pdg2212_E1e4_inj6900_z0_expon pdg2212_E1e4_inj6900_z0_interp
-    python3 analysis.py pdg2212_E1e5_lowAlt pdg2212_E1e5_inj6900_z0_expon pdg2212_E1e5_inj6900_z0_interp
+if false; then
+    python3 analysis.py pdg2212_E1e3_incl pdg2212_E1e3_inj112750_z60_expon pdg2212_E1e3_inj112750_z60_interp
+    python3 analysis.py pdg2212_E1e4_incl pdg2212_E1e4_inj112750_z60_expon pdg2212_E1e4_inj112750_z60_interp
+    python3 analysis.py pdg2212_E1e5_incl pdg2212_E1e5_inj112750_z60_expon pdg2212_E1e5_inj112750_z60_interp
 fi
