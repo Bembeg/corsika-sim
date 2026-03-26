21643630eeec91f5c018defda7922b6faeee0d19
Radek Privara <radek.privara@upol.cz>
2026-03-26 09:04:19 +0100

    add ratio plots

--- track_test.py
+++ track_test.py
@@ -18,7 +18,7 @@ os.makedirs("plots/track_tests", exist_ok=True)
 
 colors = ["red", "orange", "green", "blue", "violet", "black"]
 
-dpi_val = 300
+dpi_val = 450
 
 # add ratio columns
 df["d_gramIntSingle"] = df["gramIntSingle"] / df["gramExp"] - 1
@@ -34,157 +34,82 @@ df["da_lenIntSingle"] = abs( df["lenIntSingle"] / df["lenExp"] - 1 )
 df["da_lenInt"] = abs( df["lenInt"] / df["lenExp"] - 1 )
 df["da_lenExp"] = abs( df["lenExp"] / df["lenExp"] - 1 )
 
-# GRAMMAGE
-fig, ax = plt.subplots()
-df.plot(x="length", y="d_gramExp", label="expon. atm.", ax=ax, color=colors[0], marker=".")
-df.plot(x="length", y="d_gramIntSingle", label="interp. atm.", ax=ax, color=colors[2], marker=".", title="test tracks between R0 = 99 km and R1 = 38 km", xlabel="track length [m]",
-    ylabel = r"$\frac{integr.~gram.~(~*~atm.)}{integr.~gram.~(expon. atm.)} - 1$")
-ax.grid(ls="dashed", c="0.85")
-fig.savefig("plots/track_tests/gram_" + name + "_trLen_lin.png", dpi=dpi_val)
+# GRAMMAGE - length
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, height_ratios=[0.5, 0.5])
+plt.subplots_adjust(hspace=0.05)
+ax1.tick_params(axis='x', direction='in')
+ax2.tick_params(axis='x', direction='in', top=True)
+ax1.set_yscale("log")
+ax2.set_yscale("log")
+ax1.set_axisbelow(True)
+ax2.set_axisbelow(True)
+# main plot
+df.plot(x="length", y="gramExp", label="expon. atm.", ax=ax1, color=colors[0], marker=".")
+df.plot(x="length", y="gramIntSingle", label="interp. atm.", ax=ax1, color=colors[2], marker=".", title="test tracks between R0 = 99 km and R1 = 38 km", xlabel="track length [m]",
+    ylabel = "integr. gram. [g/cm$^2$]")
+# ratio plot
+df.plot(x="length", y="da_gramIntSingle", label=None, legend=None, ax=ax2, color=colors[2], marker=".", title=None, xlabel="track length [m]",
+    ylabel = r"$abs\left[\frac{interp. atm.}{expon. atm.} - 1\right]$")
+ax1.grid(ls="dashed", c="0.85")
+ax2.grid(ls="dashed", c="0.85")
+fig.savefig("plots/track_tests/gram_" + name + "_trLen.png", dpi=dpi_val)
 
-fig, ax = plt.subplots()
-df.plot(x="length", y="da_gramExp", label="expon. atm.", ax=ax, color=colors[0], marker=".")
-df.plot(x="length", y="da_gramIntSingle", label="interp. atm.", ax=ax, color=colors[2], marker=".", title="test tracks between R0 = 99 km and R1 = 38 km", xlabel="track length [m]",
-    ylabel = r"$abs\left[\frac{integr.~gram.~(~*~atm.)}{integr.~gram.~(expon. atm.)} - 1\right]$")
-ax.grid(ls="dashed", c="0.85")
-ax.set_yscale("log")
-fig.savefig("plots/track_tests/gram_" + name + "_trLen_log.png", dpi=dpi_val)
+# GRAMMAGE - angle
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, height_ratios=[0.5, 0.5])
+plt.subplots_adjust(hspace=0.05)
+ax1.tick_params(axis='x', direction='in')
+ax2.tick_params(axis='x', direction='in', top=True)
+ax1.set_yscale("log")
+ax2.set_yscale("log")
+ax1.set_axisbelow(True)
+ax2.set_axisbelow(True)
+# main plot
+df.plot(x="angle", y="gramExp", label="expon. atm.", ax=ax1, color=colors[0], marker=".")
+df.plot(x="angle", y="gramIntSingle", label="interp. atm.", ax=ax1, color=colors[2], marker=".", title="test tracks between R0 = 99 km and R1 = 38 km", xlabel="track zenith angle [deg]",
+    ylabel = "integr. gram. [g/cm$^2$]")
+# ratio plot
+df.plot(x="angle", y="da_gramIntSingle", label=None, legend=None, ax=ax2, color=colors[2], marker=".", title=None, xlabel="track zenith angle [deg]",
+    ylabel = r"$abs\left[\frac{interp. atm.}{expon. atm.} - 1\right]$")
+ax1.grid(ls="dashed", c="0.85")
+ax2.grid(ls="dashed", c="0.85")
+fig.savefig("plots/track_tests/gram_" + name + "_trAng.png", dpi=dpi_val)
 
-fig, ax = plt.subplots()
-df.plot(x="angle", y="d_gramExp", label="expon. atm.", ax=ax, color=colors[0], marker=".")
-df.plot(x="angle", y="d_gramIntSingle", label="interp. atm.", ax=ax, color=colors[2], marker=".", title="test tracks between R0 = 99 km and R1 = 38 km", xlabel="track zenith angle [deg]",
-    ylabel = r"$\frac{integr.~gram.~(~*~atm.)}{integr.~gram.~(expon. atm.)} - 1$")
-ax.grid(ls="dashed", c="0.85")
-fig.savefig("plots/track_tests/gram_" + name + "_trAng_lin.png", dpi=dpi_val)
+# TRACK LENGTH - length
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, height_ratios=[0.5, 0.5])
+plt.subplots_adjust(hspace=0.05)
+ax1.tick_params(axis='x', direction='in')
+ax2.tick_params(axis='x', direction='in', top=True)
+ax1.set_axisbelow(True)
+ax2.set_axisbelow(True)
+# ax1.set_yscale("log")
+ax2.set_yscale("log")
+# main plot
+df.plot(x="length", y="lenExp", label="expon. atm.", ax=ax1, color=colors[0], marker=".")
+df.plot(x="length", y="lenIntSingle", label="interp. atm.", ax=ax1, color=colors[2], marker=".", title="test tracks between R0 = 99 km and R1 = 38 km", xlabel="track length [m]",
+    ylabel = "length from gram. [m]")
+# ratio plot
+df.plot(x="length", y="da_lenIntSingle", label=None, legend=None, ax=ax2, color=colors[2], marker=".", title=None, xlabel="track length [m]",
+    ylabel = r"$abs\left[\frac{interp. atm.}{expon. atm.} - 1\right]$")
+ax1.grid(ls="dashed", c="0.85")
+ax2.grid(ls="dashed", c="0.85")
+fig.savefig("plots/track_tests/len_" + name + "_trLen.png", dpi=dpi_val)
 
-fig, ax = plt.subplots()
-df.plot(x="angle", y="da_gramExp", label="expon. atm.", ax=ax, color=colors[0], marker=".")
-df.plot(x="angle", y="da_gramIntSingle", label="interp. atm.", ax=ax, color=colors[2], marker=".", title="test tracks between R0 = 99 km and R1 = 38 km", xlabel="track zenith angle [deg]",
-    ylabel = r"$abs\left[\frac{integr.~gram.~(~*~atm.)}{integr.~gram.~(expon. atm.)} - 1\right]$")
-ax.grid(ls="dashed", c="0.85")
-ax.set_yscale("log")
-fig.savefig("plots/track_tests/gram_" + name + "_trAng_log.png", dpi=dpi_val)
-
-
-# TRACK LENGTHS
-fig, ax = plt.subplots()
-df.plot(x="length", y="d_lenExp", label="expon. atm.", ax=ax, color=colors[0], marker=".")
-df.plot(x="length", y="d_lenIntSingle", label="interp. atm.", ax=ax, color=colors[2], marker=".", title="test tracks between R0 = 99 km and R1 = 38 km", xlabel="track length [m]",
-    ylabel = r"$\frac{track~length~(~*~atm.)}{track~length~(expon. atm.)} - 1$")
-ax.grid(ls="dashed", c="0.85")
-fig.savefig("plots/track_tests/len_" + name + "_trLen_lin.png", dpi=dpi_val)
-
-fig, ax = plt.subplots()
-df.plot(x="length", y="da_lenExp", label="expon. atm.", ax=ax, color=colors[0], marker=".")
-df.plot(x="length", y="da_lenIntSingle", label="interp. atm.", ax=ax, color=colors[2], marker=".", title="test tracks between R0 = 99 km and R1 = 38 km", xlabel="track length [m]",
-    ylabel = r"$abs\left[\frac{track~length~(~*~atm.)}{track~length~(expon. atm.)} - 1\right]$")
-ax.grid(ls="dashed", c="0.85")
-ax.set_yscale("log")
-fig.savefig("plots/track_tests/len_" + name + "_trLen_log.png", dpi=dpi_val)
-
-fig, ax = plt.subplots()
-df.plot(x="angle", y="d_lenExp", label="expon. atm.", ax=ax, color=colors[0], marker=".")
-df.plot(x="angle", y="d_lenIntSingle", label="interp. atm.", ax=ax, color=colors[2], marker=".", title="test tracks between R0 = 99 km and R1 = 38 km", xlabel="track zenith angle [deg]",
-    ylabel = r"$\frac{track~length~(~*~atm.)}{track~length~(expon. atm.)} - 1$")
-ax.grid(ls="dashed", c="0.85")
-fig.savefig("plots/track_tests/len_" + name + "_trAng_lin.png", dpi=dpi_val)
-
-fig, ax = plt.subplots()
-df.plot(x="angle", y="da_lenExp", label="expon. atm.", ax=ax, color=colors[0], marker=".")
-df.plot(x="angle", y="da_lenIntSingle", label="interp. atm.", ax=ax, color=colors[2], marker=".", title="test tracks between R0 = 99 km and R1 = 38 km", xlabel="track zenith angle [deg]",
-    ylabel = r"$abs\left[\frac{track~length~(~*~atm.)}{track~length~(expon. atm.)} - 1\right]$")
-ax.grid(ls="dashed", c="0.85")
-ax.set_yscale("log")
-fig.savefig("plots/track_tests/len_" + name + "_trAng_log.png", dpi=dpi_val)
-
-
-path = "data/tracktests-full.txt"
-name = "full"
-
-# Load csv files as dataframes
-df = pd.read_csv(path)
-
-# add ratio columns
-df["d_gramIntSingle"] = df["gramIntSingle"] / df["gramExp"] - 1
-df["d_gramInt"] = df["gramInt"] / df["gramExp"] - 1
-df["d_gramExp"] = df["gramExp"] / df["gramExp"] - 1
-df["d_lenIntSingle"] = df["lenIntSingle"] / df["lenExp"] - 1
-df["d_lenInt"] = df["lenInt"] / df["lenExp"] - 1
-df["d_lenExp"] = df["lenExp"] / df["lenExp"] - 1
-df["da_gramIntSingle"] = abs( df["gramIntSingle"] / df["gramExp"] - 1 )
-df["da_gramInt"] = abs( df["gramInt"] / df["gramExp"] - 1 )
-df["da_gramExp"] = abs( df["gramExp"] / df["gramExp"] - 1 )
-df["da_lenIntSingle"] = abs( df["lenIntSingle"] / df["lenExp"] - 1 )
-df["da_lenInt"] = abs( df["lenInt"] / df["lenExp"] - 1 )
-df["da_lenExp"] = abs( df["lenExp"] / df["lenExp"] - 1 )
-
-# GRAMMAGE
-fig, ax = plt.subplots()
-df.plot(x="length", y="d_gramExp", label="expon. atm. (segments)", ax=ax, color=colors[0], marker=".")
-df.plot(x="length", y="d_gramInt", label="interp. atm. (segments)", ax=ax, color=colors[1], marker=".")
-df.plot(x="length", y="d_gramIntSingle", label="interp. atm. (one track)", ax=ax, color=colors[2], marker=".", title="test tracks between R0 = 112 km and R1 = 38 km", xlabel="track length [m]",
-    ylabel = r"$\frac{integr.~gram.~(~*~atm.)}{integr.~gram.~(expon. atm.)} - 1$")
-ax.grid(ls="dashed", c="0.85")
-fig.savefig("plots/track_tests/gram_" + name + "_trLen_lin.png", dpi=dpi_val)
-
-fig, ax = plt.subplots()
-df.plot(x="length", y="da_gramExp", label="expon. atm. (segments)", ax=ax, color=colors[0], marker=".")
-df.plot(x="length", y="da_gramInt", label="interp. atm. (segments)", ax=ax, color=colors[1], marker=".")
-df.plot(x="length", y="da_gramIntSingle", label="interp. atm. (one track)", ax=ax, color=colors[2], marker=".", title="test tracks between R0 = 112 km and R1 = 38 km", xlabel="track length [m]",
-    ylabel = r"$abs\left[\frac{integr.~gram.~(~*~atm.)}{integr.~gram.~(expon. atm.)} - 1\right]$")
-ax.grid(ls="dashed", c="0.85")
-ax.set_yscale("log")
-fig.savefig("plots/track_tests/gram_" + name + "_trLen_log.png", dpi=dpi_val)
-
-fig, ax = plt.subplots()
-df.plot(x="angle", y="d_gramExp", label="expon. atm. (segments)", ax=ax, color=colors[0], marker=".")
-df.plot(x="angle", y="d_gramInt", label="interp. atm. (segments)", ax=ax, color=colors[1], marker=".")
-df.plot(x="angle", y="d_gramIntSingle", label="interp. atm. (one track)", ax=ax, color=colors[2], marker=".", title="test tracks between R0 = 112 km and R1 = 38 km", xlabel="track zenith angle [deg]",
-    ylabel = r"$\frac{integr.~gram.~(~*~atm.)}{integr.~gram.~(expon. atm.)} - 1$")
-ax.grid(ls="dashed", c="0.85")
-fig.savefig("plots/track_tests/gram_" + name + "_trAng_lin.png", dpi=dpi_val)
-
-fig, ax = plt.subplots()
-df.plot(x="angle", y="da_gramExp", label="expon. atm. (segments)", ax=ax, color=colors[0], marker=".")
-df.plot(x="angle", y="da_gramInt", label="interp. atm. (segments)", ax=ax, color=colors[1], marker=".")
-df.plot(x="angle", y="da_gramIntSingle", label="interp. atm. (one track)", ax=ax, color=colors[2], marker=".", title="test tracks between R0 = 112 km and R1 = 38 km", xlabel="track zenith angle [deg]",
-    ylabel = r"$abs\left[\frac{integr.~gram.~(~*~atm.)}{integr.~gram.~(expon. atm.)} - 1\right]$")
-ax.grid(ls="dashed", c="0.85")
-ax.set_yscale("log")
-fig.savefig("plots/track_tests/gram_" + name + "_trAng_log.png", dpi=dpi_val)
-
-
-# TRACK LENGTHS
-fig, ax = plt.subplots()
-df.plot(x="length", y="d_lenExp", label="expon. atm. (segments)", ax=ax, color=colors[0], marker=".")
-df.plot(x="length", y="d_lenInt", label="interp. atm. (segments)", ax=ax, color=colors[1], marker=".")
-df.plot(x="length", y="d_lenIntSingle", label="interp. atm. (one track)", ax=ax, color=colors[2], marker=".", title="test tracks between R0 = 112 km and R1 = 38 km", xlabel="track length [m]",
-    ylabel = r"$\frac{track~length~(~*~atm.)}{track~length~(expon. atm.)} - 1$")
-ax.grid(ls="dashed", c="0.85")
-fig.savefig("plots/track_tests/len_" + name + "_trLen_lin.png", dpi=dpi_val)
-
-fig, ax = plt.subplots()
-df.plot(x="length", y="da_lenExp", label="expon. atm. (segments)", ax=ax, color=colors[0], marker=".")
-df.plot(x="length", y="da_lenInt", label="interp. atm. (segments)", ax=ax, color=colors[1], marker=".")
-df.plot(x="length", y="da_lenIntSingle", label="interp. atm. (one track)", ax=ax, color=colors[2], marker=".", title="test tracks between R0 = 112 km and R1 = 38 km", xlabel="track length [m]",
-    ylabel = r"$abs\left[\frac{track~length~(~*~atm.)}{track~length~(expon. atm.)} - 1\right]$")
-ax.grid(ls="dashed", c="0.85")
-ax.set_yscale("log")
-fig.savefig("plots/track_tests/len_" + name + "_trLen_log.png", dpi=dpi_val)
-
-fig, ax = plt.subplots()
-df.plot(x="angle", y="d_lenExp", label="expon. atm. (segments)", ax=ax, color=colors[0], marker=".")
-df.plot(x="angle", y="d_lenInt", label="interp. atm. (segments)", ax=ax, color=colors[1], marker=".")
-df.plot(x="angle", y="d_lenIntSingle", label="interp. atm. (one track)", ax=ax, color=colors[2], marker=".", title="test tracks between R0 = 112 km and R1 = 38 km", xlabel="track zenith angle [deg]",
-    ylabel = r"$\frac{track~length~(~*~atm.)}{track~length~(expon. atm.)} - 1$")
-ax.grid(ls="dashed", c="0.85")
-fig.savefig("plots/track_tests/len_" + name + "_trAng_lin.png", dpi=dpi_val)
-
-fig, ax = plt.subplots()
-df.plot(x="angle", y="da_lenExp", label="expon. atm. (segments)", ax=ax, color=colors[0], marker=".")
-df.plot(x="angle", y="da_lenInt", label="interp. atm. (segments)", ax=ax, color=colors[1], marker=".")
-df.plot(x="angle", y="da_lenIntSingle", label="interp. atm. (one track)", ax=ax, color=colors[2], marker=".", title="test tracks between R0 = 112 km and R1 = 38 km", xlabel="track zenith angle [deg]",
-    ylabel = r"$abs\left[\frac{track~length~(~*~atm.)}{track~length~(expon. atm.)} - 1\right]$")
-ax.grid(ls="dashed", c="0.85")
-ax.set_yscale("log")
-fig.savefig("plots/track_tests/len_" + name + "_trAng_log.png", dpi=dpi_val)
+# TRACK LENGTH - angle
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, height_ratios=[0.5, 0.5])
+plt.subplots_adjust(hspace=0.05)
+ax1.tick_params(axis='x', direction='in')
+ax2.tick_params(axis='x', direction='in', top=True)
+ax1.set_axisbelow(True)
+ax2.set_axisbelow(True)
+# ax1.set_yscale("log")
+ax2.set_yscale("log")
+# main plot
+df.plot(x="angle", y="lenExp", label="expon. atm.", ax=ax1, color=colors[0], marker=".")
+df.plot(x="angle", y="lenIntSingle", label="interp. atm.", ax=ax1, color=colors[2], marker=".", title="test tracks between R0 = 99 km and R1 = 38 km", xlabel="track zenith angle [deg]",
+    ylabel = "length from gram. [m]")
+# ratio plot
+df.plot(x="angle", y="da_lenIntSingle", label=None, legend=None, ax=ax2, color=colors[2], marker=".", title=None, xlabel="track zenith angle [deg]",
+    ylabel = r"$abs\left[\frac{interp. atm.}{expon. atm.} - 1\right]$")
+ax1.grid(ls="dashed", c="0.85")
+ax2.grid(ls="dashed", c="0.85")
+fig.savefig("plots/track_tests/len_" + name + "_trAng.png", dpi=dpi_val)
